eb0b9034bffa17b187ad608db722890f25cde93a
adrianescat <adrian.escat@gmail.com>
2015-07-10 18:50:27 -0300

    Creating partials and scrolling with -animatescroll.js-

---
+++ source/javascripts/scroll.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+	//animatescroll.js---------------------------------------------------//
+		//HEADER
+		$("#js-about-link").on("click",function(){
+			$("#js-about-section").animatescroll();
+		});
+		$("#js-studies-link").on("click",function(){
+			$("#js-studies-section").animatescroll();
+		});
+		$("#js-xp-link").on("click",function(){
+			$("#js-xp-section").animatescroll();
+		});
+		$("#js-like-link").on("click",function(){
+			$("#js-like-section").animatescroll();
+		});
+		$("#js-contact-link").on("click",function(){
+			$("#js-contact-section").animatescroll();
+		});
+
+		//FOOTER
+		$("#js-about-footer-link").on("click",function(){
+			$("#js-about-section").animatescroll();
+		});
+		$("#js-studies-footer-link").on("click",function(){
+			$("#js-studies-section").animatescroll();
+		});
+		$("#js-xp-footer-link").on("click",function(){
+			$("#js-xp-section").animatescroll();
+		});
+		$("#js-like-footer-link").on("click",function(){
+			$("#js-like-section").animatescroll();
+		});
+		$("#js-contact-footer-link").on("click",function(){
+			$("#js-contact-section").animatescroll();
+		});
+	//-------------------------------------------------------------------//
+
+
+
+});
