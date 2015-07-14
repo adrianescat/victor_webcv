bdeb3240e7a9238731acef79abf0ffbaf34c4629
adrianescat <adrian.escat@gmail.com>
2015-07-14 20:17:16 -0300

    Responsive timeline

--- source/javascripts/timeline.js
+++ source/javascripts/timeline.js
@@ -1,38 +1,18 @@
-$(document).ready(function(){
-   $("#timeline").timelinr({
-      orientation: 'horizontal',
-      // value: horizontal | vertical, default to horizontal
-      containerDiv: '#timeline',
-      // value: any HTML tag or #id, default to #timeline
-      datesDiv: '#dates',
-      // value: any HTML tag or #id, default to #dates
-      datesSelectedClass: 'selected',
-      // value: any class, default to selected
-      datesSpeed: 'normal',
-      // value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to normal
-      issuesDiv : '#issues',
-      // value: any HTML tag or #id, default to #issues
-      issuesSelectedClass: 'selected',
-      // value: any class, default to selected
-      issuesSpeed: 'fast',
-      // value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to fast
-      issuesTransparency: 0.2,
-      // value: integer between 0 and 1 (recommended), default to 0.2
-      issuesTransparencySpeed: 500,
-      // value: integer between 100 and 1000 (recommended), default to 500 (normal)
-      prevButton: '#prev',
-      // value: any HTML tag or #id, default to #prev
-      nextButton: '#next',
-      // value: any HTML tag or #id, default to #next
-      arrowKeys: 'false',
-      // value: true/false, default to false
-      startAt: 1,
-      // value: integer, default to 1 (first)
-      autoPlay: 'false',
-      // value: true | false, default to false
-      autoPlayDirection: 'forward',
-      // value: forward | backward, default to forward
-      autoPlayPause: 2000
-      // value: integer (1000 = 1 seg), default to 2000 (2segs)< });
-   });
+$(document).ready(function(e){
+
+	$('.cntl').cntl({
+
+	// The amount of "scroll padding" to allow 
+	// The more, the later the state will be revealed
+	revealbefore: 600,
+
+	// The animate class
+	// This class should have animation rules in css
+	anim_class: 'cntl-animate',
+
+	// A callback once the state has been revealed
+	onreveal: null
+
+	});
+
 });
