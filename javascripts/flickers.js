7583f9b4c4fb4c25d710138110236ee3f0f3a173
adrianescat <adrian.escat@gmail.com>
2015-07-17 22:45:48 -0300

    initial gh-pages commit

---
+++ javascripts/flickers.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){	
+    $('.flicker-studies').flickerplate({
+    	arrows: true,
+    	auto_flick_delay: 20,
+    	flick_animation: "jquery-fade",
+    	flick_position: 1,
+    	theme: "dark",
+    	block_text: false
+    });
+});
